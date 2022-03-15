33376e664a0ab7b0edaa29960280d110f82a8072
Andres <3021778410101@ingenieria.usac.edu.gt>
2022-03-15 11:49:05 -0600

    1

--- error.py
+++ error.py
@@ -1,9 +1,10 @@
 class Error: 
-    def __init__(self, descripcion: str, linea, columna):
+    def __init__(self, descripcion:str, tipo, linea, columna):
 
         self.descripcion = descripcion
         self.linea = linea
+        self.tipo = tipo
         self.columna = columna
     def imprimir_error(self):
-        print(self.descripcion, self.linea, self.columna)
+        print(self.descripcion, self.tipo, self.linea, self.columna)
         
